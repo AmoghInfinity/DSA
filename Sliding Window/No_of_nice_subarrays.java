88fc4f239f36d83896b85ed6ed9833a420c9ccad
Amogh Gupta <156122521+AmoghInfinity@users.noreply.github.com>
2026-09-15 01:55:53 +0530

    Create No_of_nice_subarrays.java

---
+++ Sliding Window/No_of_nice_subarrays.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int oddCount = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            if (num % 2 != 0) {
+                oddCount++;
+            }
+
+            if (map.containsKey(oddCount - k)) {
+                count += map.get(oddCount - k);
+            }
+
+            map.put(oddCount,
+                    map.getOrDefault(oddCount, 0) + 1);
+        }
+
+        return count;
+    }
+}
